4cae6c0987e4b291f7cf8aeb24b97bb360452c00
Daniel Zanzini <dpzanzini@gmail.com>
2015-05-25 19:54:12 -0300

    Adding seed to city and states

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+module BRPopulate
+  def self.states
+    http = Net::HTTP.new('raw.githubusercontent.com', 443); http.use_ssl = true
+    JSON.parse http.get('/celsodantas/br_populate/master/states.json').body
+  end
+
+  def self.populate
+    states.each do |state|
+      state_obj = State.new(:acronym => state["acronym"], :name => state["name"])
+      state_obj.save
+      
+      state["cities"].each do |city|
+        c = City.new
+        c.name = city
+        c.state = state_obj
+        c.save
+      end
+    end
+  end
+end
+
+BRPopulate.populate
